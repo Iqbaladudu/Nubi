bec6bbad4a9017c7a74abbb7655c3f87bd6087ba
iqbaladudu <iqbal.adudu@gmail.com>
2024-05-14 19:55:45 +0700

    remove dark

--- nubi-frontend/app/login/page.tsx
+++ nubi-frontend/app/login/page.tsx
@@ -39,11 +39,11 @@ export default function Login() {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 h-screen flex justify-center items-center">
+    <section className="bg-gray-900 h-screen flex justify-center items-center">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          className="flex items-center mb-6 text-2xl font-semibold text-white"
         >
           <Image className="w-32" src={darkLogo} alt="logo" />
         </a>
@@ -57,27 +57,27 @@ export default function Login() {
               onSubmit={handleSubmit(onSubmit)}
             >
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium text-white">
                   Email
                 </label>
                 <input
                   type="email"
                   {...register("email", { required: true })}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   placeholder="nama@emailmu.com"
                   required
                 />
               </div>
 
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium text-white">
                   Kata Sandi
                 </label>
                 <input
                   type="password"
                   {...register("password", { required: true })}
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   required
                 />
               </div>
@@ -102,11 +102,11 @@ export default function Login() {
                   )}
                 </button>
               </div>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-light text-gray-400">
                 Belum memiliki akun?{" "}
                 <Link
                   href="/register"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium hover:underline text-primary-500"
                 >
                   Daftar di sini
                 </Link>
